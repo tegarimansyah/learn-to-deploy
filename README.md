4fc3d18335f0a18617768f449cb8937b1b10d96f
Tegar Imansyah <egar.imansyah@gmail.com>
2023-11-02 08:28:19 +0000

    feat: remove deps python version

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Learn How To Deploy FastAPI in Kubernetes
 
+- AWS IAM
+    - Identity and Access Management
+        - User access
+
 - The fastapi source code is in `app` folder
 - We will try to:
     - Run it from local
@@ -7,3 +11,6 @@
     - Then deploy to AWS EC2 as docker container
     - Then deploy to Kubernetes (we will use it locally)
     - Then deploy to AWS ECS
+
+
+Temen Temen boleh install AWS CLI dulu ya.
--- deployment/ec2/init.sh
+++ deployment/ec2/init.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
 apt update
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
-apt install caddy
+apt install caddy python3-pip
+
+git clone https://github.com/tegarimansyah/learn-to-deploy.git
+cd learn-to-deploy
+pip install -r requirements.txt
